3d82511a316820df49a071f2cadf54978db5edfb
T Merrill <150313129+T-Merrill@users.noreply.github.com>
2026-08-24 02:12:52 -0700

    Added movement click indicator

--- brackeys-jam-2026.2/Levels/tmerrill_test_scene.gd
+++ brackeys-jam-2026.2/Levels/tmerrill_test_scene.gd
@@ -1,29 +1,53 @@
 extends Node3D
 
+@export var click_indicator_scene: PackedScene = preload("res://TaylorJunk/click_indicator.tscn")
+
 @onready var camera: Camera3D = $Camera3D
 @onready var player: CharacterBody3D = $Player
 
+var click_indicator: MeshInstance3D = null
 var is_mouse_held: bool = false
+
+
+func _ready() -> void:
+	if click_indicator_scene:
+		var instance := click_indicator_scene.instantiate()
+		click_indicator = instance as MeshInstance3D
+		if click_indicator:
+			add_child(click_indicator)
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		is_mouse_held = event.pressed
-		if is_mouse_held:
-			update_movement_target()
+		if event.pressed:
+			is_mouse_held = true
+			var hit_position: Vector3 = raycast_to_floor(event.position)
+			if hit_position != Vector3.INF:
+				player.set_movement_target(hit_position)
+				if click_indicator:
+					click_indicator.play_click_ping(hit_position)
+		else:
+			is_mouse_held = false
+			if click_indicator:
+				click_indicator.finish_hold()
 
 
 func _physics_process(_delta: float) -> void:
 	if is_mouse_held:
-		update_movement_target()
+		var current_mouse_position := get_viewport().get_mouse_position()
+		var hit_position: Vector3 = raycast_to_floor(current_mouse_position)
+		if hit_position != Vector3.INF:
+			player.set_movement_target(hit_position)
+			if click_indicator:
+				click_indicator.update_hold_position(hit_position)
 
 
-func update_movement_target() -> void:
+func raycast_to_floor(screen_position: Vector2) -> Vector3:
 	if not camera or not player:
-		return
+		return Vector3.INF
 	
-	var mouse_pos := get_viewport().get_mouse_position()
-	var ray_origin := camera.project_ray_origin(mouse_pos)
-	var ray_normal := camera.project_ray_normal(mouse_pos)
+	var ray_origin := camera.project_ray_origin(screen_position)
+	var ray_normal := camera.project_ray_normal(screen_position)
 	var ray_length := 1000.0
 	
 	var space_state := get_world_3d().direct_space_state
@@ -32,4 +56,6 @@ func update_movement_target() -> void:
 	
 	var result := space_state.intersect_ray(query)
 	if result:
-		player.set_movement_target(result.position)
+		return result.position
+	return Vector3.INF
+	
