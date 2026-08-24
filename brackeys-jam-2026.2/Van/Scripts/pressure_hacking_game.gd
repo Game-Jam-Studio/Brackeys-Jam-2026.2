ea902a30e19a125123f1a485cb6d3883a1201969
Vanvanderson <iloveminecraft2003@gmail.com>
2026-08-24 14:24:43 -0700

    Finished Steam Minigame Prototype
    Steam minigame:
    Finished functionality for steam minigame, this included fixing value rounding errors and adding win/loss conditions.
    Added basic graphics for gauge, needle, and correct area for steam minigame.
    
    Pressure minigame:
    Fixed and improved ease of use for pressure minigame by converting export variable to degrees from radians.

--- brackeys-jam-2026.2/Van/Scripts/pressure_hacking_game.gd
+++ brackeys-jam-2026.2/Van/Scripts/pressure_hacking_game.gd
@@ -1,29 +1,34 @@
 extends Control
 @onready var color_rect: ColorRect = $ColorRect
 @onready var accept_button: Button = $VBoxContainer/Accept
-@onready var good_job: Label = $"Good Job"
 
 var deltaTime : float = 0.0
 
-@export var tolerance: float = 0.15
+@export var tolerance: float = 10
 
 
 func _process(delta: float) -> void:
-	color_rect.rotation = deg_to_rad(180) + sin(deltaTime * 4)
+	color_rect.rotation = deg_to_rad(0) + sin(deltaTime * 4)
 	deltaTime += delta
-	if is_equal_custom(abs(color_rect.rotation), deg_to_rad(180), tolerance):
+	if is_equal_custom(abs(color_rect.rotation), deg_to_rad(0), deg_to_rad(tolerance)):
 		color_rect.color = Color.GREEN
 	else:
 		color_rect.color = Color.RED
 
 
 func _on_button_button_down() -> void:
-	if is_equal_custom(abs(color_rect.rotation), deg_to_rad(180), tolerance):
-		good_job.visible = true
+	if is_equal_custom(abs(color_rect.rotation), deg_to_rad(0), deg_to_rad(tolerance)):
+		$Conditions/Success.visible = true
+		$VBoxContainer/Accept.visible = false
 		accept_button.visible = false
 		color_rect.visible = false
+		print("YOU WIN!")
 	else:
-		print("FAILER!")
+		$Conditions/Failure.visible = true
+		$VBoxContainer/Accept.visible = false
+		accept_button.visible = false
+		color_rect.visible = false
+		print("YOU LOSE!")
 
 func is_equal_custom(a, b, tolerance):
 	return abs(a - b) <= tolerance
--- brackeys-jam-2026.2/Van/Scripts/steam_hacking_game.gd
+++ brackeys-jam-2026.2/Van/Scripts/steam_hacking_game.gd
@@ -1,34 +1,91 @@
 extends Control
-@onready var dial1: TextureRect = $Control/TextureRect
-var gauge1: float
-var gauge2: float
-var gauge3: float
+@onready var dial_1: TextureRect = $Controls/Dial1
+@onready var dial_2: TextureRect = $Controls/Dial2
+@onready var dial_3: TextureRect = $Controls/Dial3
+@onready var gauge_1_display: Control = $Gauge1
+@onready var gauge_2_display: Control = $Gauge2
+@onready var gauge_3_display: Control = $Gauge3
 
+var gauge_1: float
+var gauge_2: float
+var gauge_3: float
+var gauge_1_correct: bool
+var gauge_2_correct: bool
+var gauge_3_correct: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	gauge1 = snapped(randf_range(0, 1), 0.1)
-	gauge2 = snapped(randf_range(0, 1), 0.1)
-	gauge3 = snapped(randf_range(0, 1), 0.1)
-	print(gauge1)
-	print(gauge2)
-	print(gauge3)
+#Set random values
+	gauge_1 = snapped(randf_range(0, 1), 0.1)
+	gauge_2 = snapped(randf_range(0, 1), 0.1)
+	gauge_3 = snapped(randf_range(0, 1), 0.1)
+#Set goal gauge rotation
+	$Gauge1/Housing/ValidArea.rotation = remap(gauge_1, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
+	$Gauge2/Housing/ValidArea.rotation = remap(gauge_2, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
+	$Gauge3/Housing/ValidArea.rotation = remap(gauge_3, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
+	#print("Dial 1 ", gauge_1)
+	#print("Dial 2 ", gauge_2)
+	#print("Dial 3 ", gauge_3)
 
+func _on_dial_1_value_set(value: Variant) -> void:
+	if value == gauge_1:
+		#print("Dial1 Correct")
+		dial_1.freeze = true
+		gauge_1_correct = true
+	else:
+		#print("Dial1 Wrong")
+		gauge_1_correct = false
+
+func _on_dial_2_value_set(value: Variant) -> void:
+	if value == gauge_2:
+		#print("Dial2 Correct")
+		dial_2.freeze = true
+		gauge_2_correct = true
+	else:
+		#print("Dial2 Wrong")
+		gauge_2_correct = false
+
+func _on_dial_3_value_set(value: Variant) -> void:
+	if value == gauge_3:
+		#print("Dial3 Correct")
+		dial_3.freeze = true
+		gauge_3_correct = true
+	else:
+		#print("Dial3 Wrong")
+		gauge_3_correct = false
+
+#Player submits their answer
+func _on_accept_button_down() -> void:
+	if gauge_1_correct and gauge_2_correct and gauge_3_correct:
+		$"Conditions/Success".visible = true
+		$Controls.visible = false
+		$Menu/Accept.visible = false
+		$Gauge1.visible = false
+		$Gauge2.visible = false
+		$Gauge3.visible = false
+	#Win signal goes here
+		print("YOU WIN!")
+	else:
+		$"Conditions/Failure".visible = true
+		$Controls.visible = false
+		$Menu/Accept.visible = false
+		$Gauge1.visible = false
+		$Gauge2.visible = false
+		$Gauge3.visible = false
+		dial_1.freeze = true
+		dial_2.freeze = true
+		dial_3.freeze = true
+	#Lose signal goes here
+		print("YOU LOSE!")
+
+#Player exits minigame
+func _on_close_button_down() -> void:
+	get_tree().quit()
 
 func _on_dial_1_value_changed(value: Variant) -> void:
-	if value == gauge1:
-		print("Dial1 Correct")
-	else:
-		print("Dial1 Wrong")
-
+	$Gauge1/Housing/Needle.rotation = remap(value, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
 
 func _on_dial_2_value_changed(value: Variant) -> void:
-	if value == gauge2:
-		print("Dial2 Correct")
-	else:
-		print("Dial2 Wrong")
+	$Gauge2/Housing/Needle.rotation = remap(value, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
 
 func _on_dial_3_value_changed(value: Variant) -> void:
-	if value == gauge3:
-		print("Dial3 Correct")
-	else:
-		print("Dial3 Wrong")
+	$Gauge3/Housing/Needle.rotation = remap(value, 0, 1, -deg_to_rad(dial_1.max_rotation), deg_to_rad(dial_1.max_rotation))
