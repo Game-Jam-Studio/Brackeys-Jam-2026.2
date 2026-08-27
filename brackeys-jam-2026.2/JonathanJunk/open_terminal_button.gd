ac4257e188b7a50157345ca3c171753e56fa9a24
Jonathan <jonathanaclinton@gmail.com>
2026-08-27 10:13:36 -0700

    add call to show ship health in terminal

--- brackeys-jam-2026.2/JonathanJunk/open_terminal_button.gd
+++ brackeys-jam-2026.2/JonathanJunk/open_terminal_button.gd
@@ -1,7 +1,5 @@
 extends Button
 
-@export var popup_ui: Node
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +7,4 @@ func _ready() -> void:
 
 
 func _on_pressed() -> void:
-	popup_ui.show_next_text("SubmarineLevel")
+	PopupUI.show_ship_status()
