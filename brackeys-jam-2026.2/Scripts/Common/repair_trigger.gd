25ebbbb30824ff72eb8dba52cacbf2169643c854
T Merrill <150313129+T-Merrill@users.noreply.github.com>
2026-08-27 04:03:21 -0700

    added final health bar art

--- brackeys-jam-2026.2/Scripts/Common/repair_trigger.gd
+++ brackeys-jam-2026.2/Scripts/Common/repair_trigger.gd
@@ -7,7 +7,6 @@ signal station_repair_succeeded(system_id: String)
 
 @export_enum("Ballast", "Boiler", "Sonar", "Circuit") var system_id: String = "Ballast"
 @export var is_broken: bool = false
-@export var popup_ui: Node
 
 # Defines where the camera interpolates to
 @export var camera_focus_point: Node3D
@@ -28,7 +27,7 @@ func interact(_player: CharacterBody3D) -> void:
 		# popup_ui.launch_terminal("System in need of repair.")
 		launch_minigame_requested.emit(system_id, self)
 	else:
-		popup_ui.launch_terminal("System operational.")
+		PopupUI.launch_terminal("System operational.")
 
 
 func end_repair(success: bool) -> void:
@@ -36,8 +35,8 @@ func end_repair(success: bool) -> void:
 	# fails the minigame to not allow retries
 	is_broken = false
 	if success:
-		popup_ui.launch_terminal("System back to full operational capacity.")
+		PopupUI.launch_terminal("System back to full operational capacity.")
 		station_repair_succeeded.emit(system_id)
 	else:
-		popup_ui.launch_terminal("System degraded further, no repair action available.")
+		PopupUI.launch_terminal("System degraded further, no repair action available.")
 		station_repair_failed.emit(system_id)
