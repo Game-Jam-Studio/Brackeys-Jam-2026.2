4bb0eb07e9bb3d475fe94b65a0aa820bcf97f0f9
Jonathan <jonathanaclinton@gmail.com>
2026-08-28 10:52:24 -0700

    add area unlocks

--- brackeys-jam-2026.2/Scripts/Common/area_progression_system.gd
+++ brackeys-jam-2026.2/Scripts/Common/area_progression_system.gd
@@ -18,6 +18,9 @@ func complete_unlock_key(key: String):
 		print("Make sure you click the Add Key/Value Pair button AFTER entering values")
 		return
 	for door in area_progression_dictionary[key].doors_to_unlock:
-		door.unlock()
 		#TODO add unlock sfx
+		door.unlock()
+	if area_progression_dictionary[key].area_to_unlock >= 0 :
+		#TODO add unlock area sfx
+		GameState.unlock_next_area(area_progression_dictionary[key].area_to_unlock)
 	#TODO loop through any other changes that are needed for this next progression
