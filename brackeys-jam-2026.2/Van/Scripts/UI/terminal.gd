f9b2c927fee01e5d58292a8238cba1c7975390e9
Vanvanderson <iloveminecraft2003@gmail.com>
2026-08-29 21:27:45 -0700

    Tweaked live_main_menu and added functionality to terminal_UI
    Adjusted lighting in live_main_menu
    
    Added gamestate stat tracking to terminal_UI

--- brackeys-jam-2026.2/Van/Scripts/UI/terminal.gd
+++ brackeys-jam-2026.2/Van/Scripts/UI/terminal.gd
@@ -1,11 +1,24 @@
 extends Control
-
+@onready var ballast_health: ProgressBar = %BallastHealth
+@onready var boiler_health: ProgressBar = %BoilerHealth
+@onready var circuit_health: ProgressBar = %CircuitHealth
+@onready var sonar_health: ProgressBar = %"Sonar Health"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	GameState.connect("ballast_health_changed", _ballast_health_changed())
+	GameState.connect("boiler_health_changed", _boiler_health_changed())
+	GameState.connect("circuit_health_changed", _circuit_health_changed())
+	GameState.connect("sonar_health_changed", _sonar_health_changed())
 
+func _ballast_health_changed():
+	ballast_health.value = GameState.ballast_health
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _boiler_health_changed():
+	boiler_health.value = GameState.boiler_health
+
+func _circuit_health_changed():
+	circuit_health.value = GameState.circuit_health
+
+func _sonar_health_changed():
+	sonar_health.value = GameState.sonar_health
