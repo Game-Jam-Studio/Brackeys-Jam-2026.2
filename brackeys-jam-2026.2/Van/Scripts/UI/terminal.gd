1bdd1381e87e999bb7b9139f5a6c8f41eb07c334
Vanvanderson <iloveminecraft2003@gmail.com>
2026-08-29 21:36:41 -0700

    Tried to add functionality to terminal_UI
    Added the bones to identify core system's health values

--- brackeys-jam-2026.2/Van/Scripts/UI/terminal.gd
+++ brackeys-jam-2026.2/Van/Scripts/UI/terminal.gd
@@ -1,4 +1,7 @@
 extends Control
+
+signal terminal_closed
+
 @onready var ballast_health: ProgressBar = %BallastHealth
 @onready var boiler_health: ProgressBar = %BoilerHealth
 @onready var circuit_health: ProgressBar = %CircuitHealth
@@ -6,19 +9,26 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameState.connect("ballast_health_changed", _ballast_health_changed())
-	GameState.connect("boiler_health_changed", _boiler_health_changed())
-	GameState.connect("circuit_health_changed", _circuit_health_changed())
-	GameState.connect("sonar_health_changed", _sonar_health_changed())
+	GameState.ballast_health_changed.connect(_ballast_health_changed)
+	GameState.boiler_health_changed.connect(_boiler_health_changed)
+	GameState.circuit_health_changed.connect(_circuit_health_changed)
+	GameState.sonar_health_changed.connect(_sonar_health_changed)
 
 func _ballast_health_changed():
+	print("poop")
 	ballast_health.value = GameState.ballast_health
 
 func _boiler_health_changed():
+	print("poop")
 	boiler_health.value = GameState.boiler_health
 
 func _circuit_health_changed():
+	print("poop")
 	circuit_health.value = GameState.circuit_health
 
 func _sonar_health_changed():
+	print("poop")
 	sonar_health.value = GameState.sonar_health
+
+func _on_texture_button_pressed() -> void:
+	terminal_closed.emit()
