c26e117e4398af34aa9669cf30e199d9fdda59fc
Vanvanderson <iloveminecraft2003@gmail.com>
2026-08-30 02:35:29 -0700

    UI Pass

--- brackeys-jam-2026.2/Scripts/UI/main_menu.gd
+++ brackeys-jam-2026.2/Scripts/UI/main_menu.gd
@@ -6,11 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-
-func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/submarine.tscn")
-
-
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/options_menu.tscn")
 
@@ -21,3 +16,7 @@ func _on_quit_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/credits_menu.tscn")
+
+
+func _on_new_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/submarine.tscn")
