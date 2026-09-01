55f8bc073f5239aba3ccf33156c795ba42e7c2e5
Vanvanderson <iloveminecraft2003@gmail.com>
2026-09-01 13:02:16 -0700

    Changed Credits and Options Scenes (Unused)
    Made it so the credits and options scenes are instantiated on the main menu instead of being switched to completely which gets rid of the audio resetting on the main menu whenever you would open the credits or options scenes.

--- brackeys-jam-2026.2/Scripts/UI/main_menu.gd
+++ brackeys-jam-2026.2/Scripts/UI/main_menu.gd
@@ -1,25 +1,25 @@
 extends Control
+
 @export var new_game_scene: String = "res://Levels/submarine.tscn"
 @export var options_scene: String = "res://Levels/options_menu.tscn"
 @export var credits_scene: String = "res://Levels/credits_menu.tscn"
 var loading_scene: String = "res://Levels/loading.tscn"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file(options_scene)
-
+	var options = load(options_scene)
+	if options:
+		var options_instance = options.instantiate()
+		get_tree().current_scene.add_child(options_instance)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file(credits_scene)
+	var credits = load(credits_scene)
+	if credits:
+		var credits_instance = credits.instantiate()
+		get_tree().current_scene.add_child(credits_instance)
 
 
 func _on_new_game_pressed() -> void:
